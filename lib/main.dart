e315184cf3dd19346e3c43982c218a4d115c4cfb
Maruf Hasan <marufhasanmitul@gmail.com>
2023-06-12 21:10:51 +0600

    appBar,SingleScrollView,favBtn,Wrap,OutLine Btn

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 //Source code Follow
 main(){
-  runApp(const myApp());//Applicaton
+  runApp(const MyApp());//Applicaton
 }
 
-class myApp extends StatelessWidget{
-  const myApp({super.key});
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -28,145 +29,234 @@ class HomeActivity extends StatelessWidget{
 
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //Appbar=============
       appBar: AppBar(
-        title: Text("My First app"),
-        titleSpacing: 0,
-        toolbarHeight: 70,
-        toolbarOpacity: 1,
-        elevation: 10,
-        backgroundColor: Colors.green,
-        actions: [
-          IconButton(
-              onPressed: () {
-                MySnackbar("I am comments", context);
-              },
-              icon: Icon(Icons.comment)),
-          IconButton(
-              onPressed: () {
-                MySnackbar("I am Search", context);
-              },
-              icon: Icon(Icons.search)),
-          IconButton(
-              onPressed: () {
-                MySnackbar("I am Setting", context);
-              },
-              icon: Icon(Icons.settings)),
-          IconButton(
-              onPressed: () {
-                MySnackbar("I am Email", context);
-              },
-              icon: Icon(Icons.email)),
-        ],
+        title:Text('Photo Gallery') ,
+        centerTitle: true,
       ),
-      floatingActionButton: FloatingActionButton(
-        elevation: 10,
-        child: Icon(Icons.add),
-        onPressed: () {
-          MySnackbar("I am floting Action Buttton", context);
-        },
-        backgroundColor: Colors.amber,
-      ),
-      bottomNavigationBar:BottomNavigationBar(
-        currentIndex: 0,
-        items: [
-
-          BottomNavigationBarItem(icon: Icon(Icons.home),label: "Home"),
-          BottomNavigationBarItem(icon: Icon(Icons.message),label: "Contract"),
-          BottomNavigationBarItem(icon: Icon(Icons.person),label: "Profile"),
-
-
-        ],
-        onTap: (int index){
-          if(index==0){
-            MySnackbar("I am Home Bottom Menu", context);
-          }
-          if(index==1){
-            MySnackbar("I am Home Contract Menu", context);
-          }
-          if(index==2){
-            MySnackbar("I am Home Profile Menu", context);
-          }
-
-        },
-      ) ,
-      drawer:Drawer(
-        child:ListView(
-          children: [
-            DrawerHeader(
-              padding: EdgeInsets.all(0),
-              child: UserAccountsDrawerHeader(
-
-                decoration: BoxDecoration(color: Colors.white,),
-                accountName: Text('Maruf hasan',style: TextStyle(color: Colors.black),),
-                accountEmail: Text('info@gmail.com',style: TextStyle(color: Colors.black)) ,
-                currentAccountPicture: Image.network("https://beautifulbangladesh.gov.bd/storage/backend/uploads/1/soil.jpg",),
-                onDetailsPressed: (){
-                  MySnackbar("I am Header Details", context);
-                },
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(8),
+                child: Text(
+                  'Welcome to Photo Gallery!',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold
+                  ),
+                ),
               ),
-            ),
-            ListTile(
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                      prefixIcon: (IconButton(
+                          onPressed: () {}, icon: Icon(Icons.search))),
+                      hintText: 'Search...',
+                      border: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.black))),
+                ),
+              ),
+              Wrap(
+                alignment: WrapAlignment.spaceBetween,
+                spacing: 20,
+                runSpacing: 20,
+                children: [
+                  OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            side: BorderSide(
+                                color: Colors.white,width: 0
+                            )
+                        )
+                    ),
+                    onPressed: () {
+                     MySnackbar("Clicked on photo!",context);
+                    },
+                    child: Column(
+                      children: [
+                        Image.network('https://maruf136.000webhostapp.com/image/nature.jpg',
+                          height: 90,
+                          width: 100,),
+                        Text('Caption',style: TextStyle(fontSize: 20,color: Colors.black),)
+                      ],
+                    ),
+                  ),
+                  OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            side: BorderSide(
+                                color: Colors.white,width: 0
+                            )
+                        )
+                    ),
+                    onPressed: () {
+                      MySnackbar("Clicked on photo!",context);
+                    },
+                    child: Column(
+                      children: [
+                        Image.network('https://maruf136.000webhostapp.com/image/nature.jpg',
+                          height: 90,
+                          width: 100,),
+                        Text('Caption',style: TextStyle(fontSize: 20,color: Colors.black),)
+                      ],
+                    ),
+                  ),
+                  OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            side: BorderSide(
+                                color: Colors.white,width: 0
+                            )
+                        )
+                    ),
+                    onPressed: () {
+                      MySnackbar("Clicked on photo!",context);
+                    },
+                    child: Column(
+                      children: [
+                        Image.network('https://maruf136.000webhostapp.com/image/nature.jpg',
+                          height: 90,
+                          width: 100,),
+                        Text('Caption',style: TextStyle(fontSize: 20,color: Colors.black),)
+                      ],
+                    ),
+                  ),
+                  OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            side: BorderSide(
+                                color: Colors.white,width: 0
+                            )
+                        )
+                    ),
+                    onPressed: () {
+                      MySnackbar("Clicked on photo!",context);
+                    },
+                    child: Column(
+                      children: [
+                        Image.network('https://maruf136.000webhostapp.com/image/nature.jpg',
+                          height: 90,
+                          width: 100,),
+                        Text('Caption',style: TextStyle(fontSize: 20,color: Colors.black),)
+                      ],
+                    ),
+                  ),
+                  OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            side: BorderSide(
+                                color: Colors.white,width: 0
+                            )
+                        )
+                    ),
+                    onPressed: () {
+                      MySnackbar("Clicked on photo!",context);
+                    },
+                    child: Column(
+                      children: [
+                        Image.network('https://maruf136.000webhostapp.com/image/nature.jpg',
+                          height: 90,
+                          width: 100,),
+                        Text('Caption',style: TextStyle(fontSize: 20,color: Colors.black),)
+                      ],
+                    ),
+                  ),
+                  OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            side: BorderSide(
+                                color: Colors.white,width: 0
+                            )
+                        )
+                    ),
+                    onPressed: () {
+                      MySnackbar("Clicked on photo!",context);
+                    },
+                    child: Column(
+                      children: [
+                        Image.network('https://maruf136.000webhostapp.com/image/nature.jpg',
+                          height: 90,
+                          width: 100,),
+                        Text('Caption',style: TextStyle(fontSize: 20,color: Colors.black),)
+                      ],
+                    ),
+                  ),
 
-              leading: Icon(Icons.home),
 
-              title: Text('Home'),
+                ],
+              ),
 
-              onTap: () {
-                MySnackbar("I am Home", context);
-              },
+              ListView(
+                primary: false,
+                shrinkWrap: true,
+                children: [
+                  SizedBox(height: 10),
+                  ListTile(
+                    title: Text(
+                      'Sample Photo 1',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    subtitle: Text('Category 1'),
+                    leading: Icon(Icons.image),
+                    tileColor: Colors.black12,
+                    dense: true,
+                    contentPadding: EdgeInsets.symmetric(horizontal: 12),
+                  ),
+                  SizedBox(height: 10),
+                  ListTile(
+                    title: Text(
+                      'Sample Photo 1',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    subtitle: Text('Category 1'),
+                    leading: Icon(Icons.image),
+                    tileColor: Colors.black12,
+                    dense: true,
+                    contentPadding: EdgeInsets.symmetric(horizontal: 12),
+                  ),
+                  SizedBox(height: 10),
+                  ListTile(
+                    title: Text(
+                      'Sample Photo 1',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    subtitle: Text('Category 1'),
+                    leading: Icon(Icons.image),
+                    tileColor: Colors.black12,
+                    dense: true,
+                    contentPadding: EdgeInsets.symmetric(horizontal: 12),
+                  ),
+                ],
+              )
 
-            ),
-            ListTile(
-              leading: Icon(Icons.person),
-              title: Text('Contract'),
-              onTap: (){
-                MySnackbar("I am contract", context);
-              },
 
-            ),
-            ListTile(
-              leading: Icon(Icons.person),
-              title: Text('Profile'),
-              onTap: (){
-                MySnackbar("I am Profile", context);
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.email),
-              title: Text('Email'),
-              onTap: (){
-                MySnackbar("I am Eamil", context);
-              },
-
-            ),
-            ListTile(
-              leading: Icon(Icons.phone_android_outlined),
-              title: Text('Phone'),
-              onTap: (){
-                MySnackbar("I am Phone", context);
-              },
-            ),
-          ],
+            ],
+          ),
         ),
       ),
 
-      //Body===============
-      body:Container(
-        width: double.infinity,
-        height: double.infinity,
-        decoration: BoxDecoration(
-          color: Colors.grey,
-        ),
-        child: Center(child: Text('This is Our Home Page',style: TextStyle(color: Colors.white,fontSize: 30),)),
+
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          MySnackbar("Uploaded Successfully!",context);
+        },
+        child: Icon(Icons.cloud_upload),
+
       ),
-
-
-
 
 
 
@@ -175,3 +265,4 @@ class HomeActivity extends StatelessWidget{
 
   }
 }
+
