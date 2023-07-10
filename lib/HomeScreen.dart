685ca3004b4a0c72268e0585fe456fc0dd32ba9f
Maruf Hasan <marufhasanmitul@gmail.com>
2023-07-10 11:56:58 +0600

    Module_9_live_assignment_Weather_app

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -1,5 +1,10 @@
+import 'dart:convert';
+
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+
 
 class HomeScreen extends StatefulWidget{
   const HomeScreen({super.key});
@@ -12,14 +17,70 @@ class HomeScreen extends StatefulWidget{
 class HomeScreenUI extends State<HomeScreen>{
 
 
+  Map<String,dynamic>weatherData={};
+  var currentTime="${DateTime.now().hour} : ${DateTime.now().minute } ";
+
+  bool inProgress=false;
 
 
-  List<Contact>contacts =[
-    Contact("John Doe", "johndoe@example.com","1111111"),
-    Contact("Jane Smith","janesmith@example.com","222222"),
-    Contact("Allice Johnson", "allicejohnson@example.com","33333"),
-    Contact("Maruf Hasan", "marufhasan136@gmail.com","33333"),
-  ];
+
+
+
+
+  
+  
+  
+
+
+  @override
+  void initState() {
+    super.initState();
+    getWeather();
+
+  }
+
+
+
+  void getWeather() async{
+    inProgress=true;
+
+    setState(() {});
+
+    Response response=await get(Uri.parse('https://api.openweathermap.org/data/2.5/weather?lat=25.743893&lon=89.275230&appid=a2993c7860fc7adb9a695772714bbc81'));
+    final Map<String, dynamic> decodeResponse = jsonDecode(response.body);
+
+
+    if(response.statusCode==200){
+
+       var currentLocation=decodeResponse['name'];
+       var currentWeatherConditions=decodeResponse['weather'][0]['main'];
+       var icon=decodeResponse['weather'][0]['icon'];
+       var temperature=decodeResponse['main']['temp'];
+       var mintemperature=decodeResponse['main']['temp_min'];
+       var maxtemperature=decodeResponse['main']['temp_max'];
+
+       weatherData["currentLocation"]=currentLocation;
+       weatherData["currentWeatherConditions"]=currentWeatherConditions;
+       weatherData["icon"]="https://openweathermap.org/img/wn/${icon}.png";
+       weatherData["temperature"]=temperature-273.15;
+       weatherData["mintemperature"]=mintemperature-273.15;
+       weatherData["maxtemperature"]=maxtemperature-273.15;
+
+       inProgress=false;
+       if(mounted){
+         setState(() {
+
+         });
+       }
+
+
+
+    }
+
+  }
+
+
+
 
 
 
@@ -27,30 +88,81 @@ class HomeScreenUI extends State<HomeScreen>{
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Contact List'),
-        centerTitle: true,
+        title: const Text('Weather'),
+
+        actions: [
+          Icon(Icons.settings),
+          SizedBox(width: 10,),
+          Icon(Icons.add),
+
+        ],
+
       ),
+      body:inProgress? Center(child: CircularProgressIndicator(),):
+      Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors: [
+                  Colors.blue,
+                  Colors.red,
+                ]
+            )
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(weatherData['currentLocation'],style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white
+              ),),
+              SizedBox(height: 15),
+              Text("Updated Time ${currentTime}",style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.white
+              ),),
+              SizedBox(height: 30,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
 
-      body: Container(
-        color: Colors.black12,
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: ListView.builder(
-              itemCount: contacts.length,
-              itemBuilder: (context,index){
-                return Card(
-                  child: ListTile(
-                    title: Text("${contacts[index].name}"),
-                    onTap:(){
-                      showBottomSheet(index);
-                    } ,
+                children: [
+                  Image.network(weatherData["icon"],),
+                  Text("${weatherData['temperature'].round()}°C ",style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30
+                  ),),
+                  Column(
+                    children: [
+                      Text("Min temp :${weatherData['mintemperature'].round()}°C ",style: TextStyle(
+                          color: Colors.white
+                      ),),
+                      SizedBox(height: 5,),
+                      Text("Max temp :${weatherData['mintemperature'].round()}°C ",style: TextStyle(color: Colors.white),)
+                    ],
                   ),
-                );
-              }
 
+
+                ],
+              ),
+              SizedBox(height: 20,),
+              Text(weatherData["currentWeatherConditions"],style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white
+              ),)
+            ],
           ),
         ),
       ),
+
+
+
+
+
     );
   }
 
@@ -59,45 +171,9 @@ class HomeScreenUI extends State<HomeScreen>{
 
 
 
-  void showBottomSheet(int indexId ){
-    showModalBottomSheet(
-        context: context,
-        builder: (context){
-          return Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Container(
-              height: 230,
-              child: SingleChildScrollView(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    const Text("Contact's Details,",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
-                    const SizedBox(height: 10),
-                    Text("Name : "+contacts[indexId].name,style: TextStyle(fontSize: 16),),
-                    const SizedBox(height: 10),
-                    Text("Email address : "+contacts[indexId].email,style: TextStyle(fontSize: 16),),
-                    const SizedBox(height: 10),
-                    Text("phone number: "+contacts[indexId].phoneNumber,style: TextStyle(fontSize: 16),),
 
-
-                  ],
-                ),
-              ),
-            ),
-          );
-        }
-    );
-  }
 
 
 
 }
 
-class Contact {
-  String name;
-  String email;
-  String phoneNumber;
-
-  Contact(this.name, this.email, this.phoneNumber);
-}
--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: "Assignment 8 Live Test",
+      title: "Assignment  9",
       debugShowCheckedModeBanner:false,
       home: HomeScreen(),
     );
